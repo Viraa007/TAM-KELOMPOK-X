4dc379c9b47d7187ffd7ba52acc5617d20abe862
Viraa007 <viraamghvira@gmail.com>
2021-11-17 21:16:42 +0700

    commit 2

--- app/src/main/java/com/kelompokx/todonote/FiturList.java
+++ app/src/main/java/com/kelompokx/todonote/FiturList.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class FiturList extends AppCompatActivity {
+public class FiturList extends AppCompatActivity implements DialogCloseListener{
     private RecyclerView tasksRecyclerView;
     private ToDoAdapter tugasAdapter;
     private FloatingActionButton fab;
